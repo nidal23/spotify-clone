1298634026d649cd1a9cee11ba9e4672345fdf53
Nidal Mohammed <nidalmo23@gmail.com>
2023-03-30 01:57:21 +0530

    added auth

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,15 @@
+import { useSession, signOut } from "next-auth/react";
 import React from "react";
 
 const Sidebar = () => {
+
+  const { data: session, status } = useSession()
+
+  console.log({ session })
   return (
     <div>
       <div className="flex flex-col gap-3 text-gray-400 p-5 text-sm border-r border-gray-900">
-        <div className="flex items-center gap-2 hover:text-white cursor-pointer">
+        <div className="flex items-center gap-2 hover:text-white cursor-pointer" onClick={() => signOut()}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -19,7 +24,7 @@ const Sidebar = () => {
               d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
             />
           </svg>
-          Home
+          Sign Out
         </div>
         <div className="flex items-center gap-2 hover:text-white cursor-pointer">
           <svg
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,78 @@
+import spotifyApi, { LOGIN_URL } from "@/lib/spotify"
+import NextAuth from "next-auth"
+import SpotifyProvider from "next-auth/providers/spotify"
+
+async function refresAccessToken(token) {
+    try {
+        spotifyApi.setAccessToken(token.accessToken);
+        spotifyApi.setRefreshToken(token.refreshToken);
+
+        const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+        console.log("REFRESHED_TOKEN", refreshedToken)
+
+        return {
+            ...token,
+            accessToken: refreshedToken.access_token,
+            accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000, //= 1 hour
+            refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+        }
+    } catch (err) {
+        console.log(err)
+
+        return {
+            ...token,
+            error: "RefreshAccessTokenError"
+        }
+    }
+}
+
+
+export default NextAuth({
+    // Configure one or more authentication providers
+    providers: [
+        SpotifyProvider({
+            clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+            clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+            authorization: LOGIN_URL,
+        }),
+        // ...add more providers here
+    ],
+    secret: process.env.JWT_SECRET,
+    pages: {
+        signIn: '/login'
+    },
+    callbacks: {
+        async jwt({ token, account, user }) {
+
+            if (account && user) {
+                return {
+                    ...token,
+                    accessToken: account.access_token,
+                    refreshToken: account.refresh_token,
+                    username: account.providerAccountId,
+                    accessTokenExpires: account.expires_at * 1000,
+                }
+            }
+
+            //return previous token if access token has not expired
+
+            if (Date.now() < token.accessTokenExpires) {
+                console.log("Access token exists")
+                return token;
+            }
+
+            //Access token has expired, refresh the token
+            console.log("Access token has expired")
+            return await refresAccessToken(token)
+        },
+
+        async session({ session, token }) {
+            session.user.accessToken = token.accessToken;
+            session.user.refreshToken = token.refreshToken;
+            session.user.username = token.username;
+
+            return session;
+        }
+    }
+})
+// export default NextAuth(authOptions)
